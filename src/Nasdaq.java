a56e5db3914a228d2891f50b4a6b5a7f6a6b1b1b
j-a-r-i <jojanen@elisanet.fi>
2018-01-06 23:12:01 +0200

    nasdaq: initial response (xml) parsing

--- src/Nasdaq.java
+++ src/Nasdaq.java
@@ -1,9 +1,43 @@
 import java.net.URL;
+import javax.xml.namespace.QName;
+import javax.xml.stream.XMLEventReader;
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.events.Attribute;
+import javax.xml.stream.events.StartElement;
+import javax.xml.stream.events.XMLEvent;
 import java.net.HttpURLConnection;
 import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
 import java.io.InputStreamReader;
 import java.io.IOException;
 import java.lang.StringBuilder;
+
+class NasdaqData {
+	private String company;
+	private float value;
+
+	public NasdaqData() {
+		company = null;
+		value = 0.0f;
+	}
+
+	public void setValue(float val) {
+		value = val;
+	}
+
+	public float getValue() {
+		return value;
+	}
+
+	public void setCompany(String val) {
+		company = val;
+	}
+
+	public String getCompany() {
+		return company;
+	}
+}
 
 public class Nasdaq {
     public static final int NOKIA = 24311;
@@ -38,6 +72,40 @@ public class Nasdaq {
 		return sb.toString();
     }
     
+    /** Parse XML output from the API.
+     *  This is public for unit test.
+     *  TODO refactor this method!
+     */
+    public NasdaqData parseXml(String str) {
+    	NasdaqData data = new NasdaqData();
+    	XMLInputFactory factory = XMLInputFactory.newInstance();
+    	ByteArrayInputStream stream = new ByteArrayInputStream(str.getBytes());
+    	try {
+			XMLEventReader reader = factory.createXMLEventReader(stream);
+			while (reader.hasNext()) {
+				XMLEvent event = reader.nextEvent();
+				if (event.isStartElement()) {
+					StartElement element = event.asStartElement();
+
+					System.out.println(element.getName().getLocalPart());
+					if (element.getName().getLocalPart().equals("inst")) {
+						Attribute attr = element.getAttributeByName(new QName("nm"));
+						if (attr != null) {
+							data.setCompany(attr.getValue());
+						}
+						attr = element.getAttributeByName(new QName("cp"));
+						if (attr != null) {
+							data.setValue(Float.parseFloat(attr.getValue()));
+						}
+					}
+				}
+			}
+		} catch (XMLStreamException e) {
+			e.printStackTrace();
+		}
+    	return data;
+    }
+
     public void download(int hexCode) {
 		System.out.println("download Nasdaq " + hexCode);
 		try {
