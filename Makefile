e466bb32fe3e4e7dedbb07d71b10528422d89e17
j-a-r-i <jojanen@elisanet.fi>
2018-01-20 20:49:13 +0200

    Only one object to handle data queries: /data/... -> /table?key=...

--- Makefile
+++ Makefile
@@ -1,7 +1,10 @@
+JAR_DIR=/mnt/work
+JAR_DIR=/home/git/lib
+
 
 all:
-	javac -cp /mnt/work/jedis-2.9.0.jar -d bin src/*.java
+	javac -cp ${JAR_DIR}/jedis-2.9.0.jar -d bin src/*.java
 
 run:
 
-	java -cp /mnt/work/jedis-2.9.0.jar:bin -Djava.util.logging.SimpleFormatter.format='%1$tY-%1$tm-%1$td %1$tH:%1$tM:%1$tS %4$s %2$s %5$s%6$s%n' JHaServer
+	java -cp "${JAR_DIR}/jedis-2.9.0.jar;bin" -Djava.util.logging.SimpleFormatter.format='%1$tY-%1$tm-%1$td %1$tH:%1$tM:%1$tS %4$s %2$s %5$s%6$s%n' JHaServer
--- src/JHaServer.java
+++ src/JHaServer.java
@@ -5,6 +5,9 @@ import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
 import com.sun.net.httpserver.Headers;
+
+import java.util.HashMap;
+import java.util.Map;
 import java.util.logging.Logger;
 //import static Tag;
 import java.util.stream.Collectors;
@@ -14,25 +17,41 @@ import java.net.URI;
 abstract class BaseHandler implements HttpHandler {
     private final static Logger LOGGER = Logger.getLogger(BaseHandler.class.getName());
     private String path;
+    protected Map<String, String> queryParam;
 	
     public BaseHandler(String path) {
-	this.path = path;
+    	this.path = path;
     }
 
     @Override
     public void handle(HttpExchange t) throws IOException {
-	URI uri = t.getRequestURI();
+		URI uri = t.getRequestURI();
+		String query = t.getRequestURI().getQuery();
+		
+    	queryParam = new HashMap<String, String>();
+    	if ((query != null) && (query.length() > 0)) {
+	    	for (String param : query.split("&")) {
+	    		String data[] = param.split("=");
+	    		
+	    		if (data.length > 1) {
+	    			queryParam.put(data[0], data[1]);
+	    		}
+	    		else {
+	    			queryParam.put(data[0], "");
+	    		}
+	    	}
+    	}
+		
+		LOGGER.info("MyHandler:"+path + " path:" + uri.getPath());
 	
-	LOGGER.info("MyHandler:"+path + " path:" + uri.getPath());
-
-	String response = content();
-	Headers h = t.getResponseHeaders();
-	h.set("Content-Type","text/html");
-	
-	t.sendResponseHeaders(200, response.length());
-	OutputStream os = t.getResponseBody();
-	os.write(response.getBytes());
-	os.close();
+		String response = content();
+		Headers h = t.getResponseHeaders();
+		h.set("Content-Type","text/html");
+		
+		t.sendResponseHeaders(200, response.length());
+		OutputStream os = t.getResponseBody();
+		os.write(response.getBytes());
+		os.close();
     }
 
     abstract String content();
@@ -43,34 +62,41 @@ abstract class BaseHandler implements HttpHandler {
 }
 
 //------------------------------------------------------------------------------
-class RedisHandler extends BaseHandler {
-    private final static Logger LOGGER = Logger.getLogger(RedisHandler.class.getName());
+class TableHandler extends BaseHandler {
+    private final static Logger LOGGER = Logger.getLogger(TableHandler.class.getName());
     private MyRedis redis;
-    private String key;
 
-    public RedisHandler(String rkey, MyRedis r) {
-	super("/data/"+rkey);
-	redis = r;
-	key = rkey;
+    public TableHandler(MyRedis r) {
+		super("/table");
+		redis = r;
     }
 
     private String row(String line) {
-	//LOGGER.info(line);
-	String[] values = line.split(",");
-	return Tag.tag("tr",
-		       Tag.tag("td", values[0]),
-		       Tag.tag("td", values[1]));
+		//LOGGER.info(line);
+		String[] values = line.split(",");
+		return Tag.tag("tr",
+			       Tag.tag("td", values[0]),
+			       Tag.tag("td", values[1]));
     }
 
     @Override
     public String content() {
-	String data = Tag.tag("div",
-			      Tag.tag("h1", "Redis " + key),
-			      Tag.tag("table",
-				      redis.values(key).stream()
-				      .map(this::row)
-				      .collect(Collectors.joining(""))
-				      ));
+    	if (!queryParam.containsKey("key")) {
+    		return "missing key";
+    	}
+
+    	String key = queryParam.get("key");
+    	if (!redis.keys().contains(key)) {
+    		return "invalid key";
+    	}
+    	
+		String data = Tag.tag("div",
+				      Tag.tag("h1", "Redis " + key),
+				      Tag.tag("table",
+					      redis.values(key).stream()
+					      .map(this::row)
+					      .collect(Collectors.joining(""))
+					      ));
     	return data;
     }
 }
@@ -78,12 +104,15 @@ class RedisHandler extends BaseHandler {
 //------------------------------------------------------------------------------
 class MyHandler1 extends BaseHandler {
     public MyHandler1() {
-	super("/one");
+    	super("/test");
     }
 	
     @Override
     public String content() {
-    	return "This is one.";
+    	if (queryParam.containsKey("key")) {
+    		return "The key is " + queryParam.get("key");
+    	}
+    	return "There is no key!";
     }
 }
 
@@ -96,7 +125,7 @@ class LinkHandler extends BaseHandler {
 
 	linkStr = redis.keys().stream()
 	    .map(i -> Tag.tag("li",
-			      Tag.a("/data/"+i, i)))
+			      Tag.a("/table?key="+i, i)))
 	    .collect(Collectors.joining(""));
 
     }
@@ -117,36 +146,33 @@ public class JHaServer {
     private static Logger LOGGER = null;
 
     static {
-	System.setProperty("java.util.logging.SimpleFormatter.format",
-			   "[%1$tF %1$tT] [%4$-7s] %5$s %n");
-	LOGGER = Logger.getLogger(JHaServer.class.getName());
+		System.setProperty("java.util.logging.SimpleFormatter.format",
+				   "[%1$tF %1$tT] [%4$-7s] %5$s %n");
+		LOGGER = Logger.getLogger(JHaServer.class.getName());
     }
     
     public static void main(String[] args) {
-	LOGGER.info("started");
+    	LOGGER.info("started");
         Config.load();
-	MyRedis redis = new MyRedis();
+        MyRedis redis = new MyRedis();
 
-	BaseHandler[] handlers = {
-	    new MyHandler1(),
-	    new LinkHandler(redis)
-	};
+		BaseHandler[] handlers = {
+		    new MyHandler1(),
+		    new LinkHandler(redis),
+		    new TableHandler(redis)
+		};
 
         HttpServer server;
-	try {
-	    server = HttpServer.create(new InetSocketAddress(Config.getServerPort()), 0);
-	    for (BaseHandler h : handlers) {
-		server.createContext(h.getPath(), h);
-	    }
-	    for (String key : redis.keys()) {
-		RedisHandler h = new RedisHandler(key, redis);
-		server.createContext(h.getPath(), h);
-	    }
-	    server.setExecutor(null); // creates a default executor
-	    server.start();
-	} catch (IOException e) {
-	    e.printStackTrace();
-	}
+		try {
+		    server = HttpServer.create(new InetSocketAddress(Config.getServerPort()), 0);
+		    for (BaseHandler h : handlers) {
+		    	server.createContext(h.getPath(), h);
+		    }
+		    server.setExecutor(null); // creates a default executor
+		    server.start();
+		} catch (IOException e) {
+		    e.printStackTrace();
+		}
 
 	//LOGGER.info("close redis");
 	//redis.close();
